d214914a061f3adca8a08426d562f5ddead45419
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2019-01-02 16:00:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,10 +3,9 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
-
+  var lyric = "maybe";
 
 function crazy() {
   // fix the code in here:
