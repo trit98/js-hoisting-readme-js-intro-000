42a29b03289a1371d43ff814f5b4f83fd9ae64e4
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2019-01-02 16:05:16 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,7 +10,7 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  var thisIsCrazy = function (){
+  var thisIsCrazy = function() {
     console.log("hey!!!")
   }
   thisIsCrazy();
